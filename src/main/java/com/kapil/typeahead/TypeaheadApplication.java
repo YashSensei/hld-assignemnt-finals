3027576dbcf34d7e6de39f8c328a2a570d877def
YashSensei <yashagrawalrkt123@gmail.com>
2026-06-21 16:29:32 +0530

    chore: bootstrap spring boot project

---
+++ src/main/java/com/kapil/typeahead/TypeaheadApplication.java
@@ -0,0 +1,15 @@
+package com.kapil.typeahead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TypeaheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeaheadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapil/typeahead/TypeaheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapil.typeahead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeaheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
